ad6dd29a960a9d7a1d64676a3b079a28f97b6ab1
eric sciple <ericsciple@users.noreply.github.com>
2019-12-09 16:54:42 -0500

    fallback to REST API to download repo

---
+++ src/repository-api-helper.ts
@@ -0,0 +1,24 @@
+import * as github from '@actions/github'
+import {ReposGetArchiveLinkParams} from '@octokit/rest'
+
+const IS_WINDOWS = process.platform === 'win32'
+
+export async function download(
+  accessToken: string,
+  owner: string,
+  repo: string,
+  ref: string,
+  repositoryPath: string
+): Promise<void> {
+  const octokit = new github.GitHub(accessToken)
+  const params: ReposGetArchiveLinkParams = {
+    archive_format: IS_WINDOWS ? 'zipball' : 'tarball',
+    owner: owner,
+    repo: repo,
+    ref: ref
+  }
+  const response = await octokit.repos.getArchiveLink(params)
+  console.log(`status=${response.status}`)
+  console.log(`headers=${JSON.stringify(response.headers)}`)
+  console.log(`data=${JSON.stringify(response.data)}`)
+}
