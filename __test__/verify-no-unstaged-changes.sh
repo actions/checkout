5f598e1f2c7fc12bea0c48aafd094894db1b9d3b
Daniel Hwang <danielleehwang@gmail.com>
2020-05-29 16:22:45 -0700

    build because all is no more

--- __test__/verify-no-unstaged-changes.sh
+++ __test__/verify-no-unstaged-changes.sh
@@ -12,6 +12,6 @@ if [[ "$(git status --porcelain)" != "" ]]; then
     echo ----------------------------------------
     echo Troubleshooting
     echo ----------------------------------------
-    echo "::error::Unstaged changes detected. Locally try running: git clean -ffdx && npm ci && npm run all"
+    echo "::error::Unstaged changes detected. Locally try running: git clean -ffdx && npm ci && npm run build"
     exit 1
 fi
