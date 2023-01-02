b800b38ab2b6b498c12cb66cc1caddb94b00a157
Nishant Sikarwar <nsikarwar@ch.iitr.ac.in>
2023-01-02 06:53:21 +0000

    removed aliasing of 'this' to local variable

--- src/git-command-manager.ts
+++ src/git-command-manager.ts
@@ -223,9 +223,8 @@ class GitCommandManager {
       args.push(arg)
     }
 
-    const that = this
     await retryHelper.execute(async () => {
-      await that.execGit(args)
+      await this.execGit(args)
     })
   }
 
@@ -277,9 +276,8 @@ class GitCommandManager {
   async lfsFetch(ref: string): Promise<void> {
     const args = ['lfs', 'fetch', 'origin', ref]
 
-    const that = this
     await retryHelper.execute(async () => {
-      await that.execGit(args)
+      await this.execGit(args)
     })
   }
 
