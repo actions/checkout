55b0bfb9c850818c73fc67177f6fed504153fc3f
Daniel <daniel.fernandez@feverup.com>
2023-05-02 20:14:03 +0200

    feat: added __test__ as sparse-checkout directory in order to be able to have the test scripts

--- __test__/verify-sparse-checkout.sh
+++ __test__/verify-sparse-checkout.sh
@@ -4,10 +4,24 @@
 for pattern in $(git ls-tree --name-only HEAD)
 do
   if [ -d "$pattern" ]; then
-    if [[ "$pattern" != ".github" && "$pattern" != "src" ]]; then
+    if [[ "$pattern" != "__test__" && "$pattern" != ".github" && "$pattern" != "src" ]]; then
       echo "Expected directory '$pattern' to not exist"
       exit 1
     fi
+  fi
+done
+
+# Check that .github and its childrens has been fetched correctly
+if [ ! -d "./__test__" ]; then
+  echo "Expected directory '__test__' to exist"
+  exit 1
+fi
+
+for file in $(git ls-tree -r --name-only HEAD __test__)
+do
+  if [ ! -f "$file" ]; then
+    echo "Expected file '$file' to exist"
+    exit 1
   fi
 done
 
