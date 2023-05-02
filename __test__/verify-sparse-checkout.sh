e12b86ed416ffa3e0c5e065186d05bb10b04a523
Daniel <daniel.fernandez@feverup.com>
2023-05-02 20:40:47 +0200

    feat: updated test to make all checks in only one check

--- __test__/verify-sparse-checkout.sh
+++ __test__/verify-sparse-checkout.sh
@@ -4,7 +4,7 @@
 for pattern in $(git ls-tree --name-only HEAD)
 do
   if [ -d "$pattern" ]; then
-    if [[ "$pattern" != "__test__" && "$pattern" != ".github" && "$pattern" != "src" ]]; then
+    if [[ "$pattern" != "__test__" && "$pattern" != ".github" && "$pattern" != "dist" ]]; then
       echo "Expected directory '$pattern' to not exist"
       exit 1
     fi
@@ -39,13 +39,13 @@ do
   fi
 done
 
-# Check that src and its childrens has been fetched correctly
-if [ ! -d "./src" ]; then
-  echo "Expected directory 'src' to exist"
+# Check that dist and its childrens has been fetched correctly
+if [ ! -d "./dist" ]; then
+  echo "Expected directory 'dist' to exist"
   exit 1
 fi
 
-for file in $(git ls-tree -r --name-only HEAD src)
+for file in $(git ls-tree -r --name-only HEAD dist)
 do
   if [ ! -f "$file" ]; then
     echo "Expected file '$file' to exist"
