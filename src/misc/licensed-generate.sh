fe8f3529c11414cb4fb23f8ccc926413cec4319b
snmb22 <160770537+snmb22@users.noreply.github.com>
2024-05-22 05:45:02 -0400

    seep

--- src/misc/licensed-generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-src/misc/licensed-download.sh
-
-echo 'Running: licensed cached'
-_temp/licensed-3.6.0/licensed cache
--- src/regexp-helper.ts
+++
@@ -1,5 +0,0 @@
-export function escape(value: string): string {
-  return value.replace(/[^a-zA-Z0-9_]/g, x => {
-    return `\\${x}`
-  })
-}
