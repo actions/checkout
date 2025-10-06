f0aad2b562860571750ecf94235d4086a78de394
andreanadr <121737246+andreanadr@users.noreply.github.com>
2025-10-06 13:14:20 +0700

    Delete __test__ directory

--- __test__/override-git-version.cmd
+++
@@ -1,6 +0,0 @@
-
-mkdir override-git-version
-cd override-git-version
-echo @echo override git version 1.2.3 > git.cmd
-echo "%CD%" >> $GITHUB_PATH
-cd ..
--- __test__/override-git-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir override-git-version
-cd override-git-version
-echo "#!/bin/sh" > git
-echo "echo override git version 1.2.3" >> git
-chmod +x git
-echo "$(pwd)" >> $GITHUB_PATH
-cd ..
